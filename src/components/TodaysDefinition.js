db4ce162391302e4ef0259ea6b209cbffda5232a
George <georgedumontier@gmail.com>
2019-03-25 20:14:30 -0500

    More words added to json file

--- src/components/TodaysDefinition.js
+++ src/components/TodaysDefinition.js
@@ -1,31 +1,29 @@
 import React from "react";
 import words from  '../words.json';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
- 
+
 class TodaysDefinition extends React.Component{
     // exitRight = function(node){
     //     node.classList.remove("prev-defs-transition-exit","prev-defs-transition-exit-active");
     //     node.classList.add("next-defs-transition-exit","next-defs-transition-exit-active");
     //     console.log(node.classList);
     // }
-    
+
     render(){
         let {day,nextOrPrev} = this.props;
         let {def} = words[day - 1 ];
-        let transitionClass = "defs-transition";
+        let transitionClass = "defs";
         if(nextOrPrev === -1){
-            transitionClass = "prev-defs-transition";
+            transitionClass = "prev-defs";
         }
-        else{transitionClass = "next-defs-transition";}
-        console.log(transitionClass);
+        else{transitionClass = "next-defs";}
         return(
             <div className="def-container">
                 <TransitionGroup component={null}>
                     <CSSTransition
-                    classNames="prev-defs-transition"
+                    classNames={transitionClass}
                     key={this.props.day}
-                    timeout={{enter:300, exit:300}} 
-                    onExit={(node) => this.exitRight(node)}>
+                    timeout={{enter:300, exit:300}} >
                         <ol className="defs">
                             {def.map((def,idx) => (<li key={idx} >{def}</li>))}
                         </ol>
